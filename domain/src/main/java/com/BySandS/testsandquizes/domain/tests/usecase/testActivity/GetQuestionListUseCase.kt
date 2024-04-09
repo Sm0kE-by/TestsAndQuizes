d73d107736d5dd70f612e3c8ef3115c8b35b1ce5
Smoke <Necrossby@gmail.com>
2024-04-09 13:11:10 +0300

    coMMit before entering a new database5

--- domain/src/main/java/com/BySandS/testsandquizes/domain/tests/usecase/testActivity/GetQuestionListUseCase.kt
+++ domain/src/main/java/com/BySandS/testsandquizes/domain/tests/usecase/testActivity/GetQuestionListUseCase.kt
@@ -6,7 +6,107 @@ import com.BySandS.testsandquizes.domain.tests.repository.TestQuestionRepository
 
 class GetQuestionListUseCase(private val tesQuestionRepository: TestQuestionRepository) {
 
-    fun execute(param: GetQuestionListParam): List<QuestionModel>{
-        return tesQuestionRepository.getQuestionsList(param = param)
+
+    fun execute(param: GetQuestionListParam): List<QuestionModel> {
+        return checkDifficulty(param = param)
+    }
+
+    private fun checkDifficulty(param: GetQuestionListParam): List<QuestionModel> {
+
+        val listQuestion = mutableListOf<QuestionModel>()
+        when (param.difficultyId) {
+            1L -> {
+                listQuestion.addAll(
+                    tesQuestionRepository.getQuestionsList(
+                        param = GetQuestionListParam(
+                            difficultyId = param.difficultyId,
+                            quantityOfQuestions = param.quantityOfQuestions - 1
+                        )
+                    )
+                )
+                listQuestion.addAll(
+                    tesQuestionRepository.getQuestionsList(
+                        param = GetQuestionListParam(
+                            difficultyId = param.difficultyId + 1L,
+                            quantityOfQuestions = param.quantityOfQuestions - (param.quantityOfQuestions - 1)
+                        )
+                    )
+                )
+            }
+
+            2L -> {
+                listQuestion.addAll(
+                    tesQuestionRepository.getQuestionsList(
+                        param = GetQuestionListParam(
+                            difficultyId = param.difficultyId - 1L,
+                            quantityOfQuestions = param.quantityOfQuestions - (param.quantityOfQuestions - 1)
+                        )
+                    )
+                )
+                listQuestion.addAll(
+                    tesQuestionRepository.getQuestionsList(
+                        param = GetQuestionListParam(
+                            difficultyId = param.difficultyId,
+                            quantityOfQuestions = param.quantityOfQuestions - (param.quantityOfQuestions - 3)
+                        )
+                    )
+                )
+                listQuestion.addAll(
+                    tesQuestionRepository.getQuestionsList(
+                        param = GetQuestionListParam(
+                            difficultyId = param.difficultyId + 1L,
+                            quantityOfQuestions = param.quantityOfQuestions - (param.quantityOfQuestions - 2)
+                        )
+                    )
+                )
+            }
+
+            3L -> {
+                listQuestion.addAll(
+                    tesQuestionRepository.getQuestionsList(
+                        param = GetQuestionListParam(
+                            difficultyId = param.difficultyId - 1L,
+                            quantityOfQuestions = param.quantityOfQuestions - (param.quantityOfQuestions - 2)
+                        )
+                    )
+                )
+                listQuestion.addAll(
+                    tesQuestionRepository.getQuestionsList(
+                        param = GetQuestionListParam(
+                            difficultyId = param.difficultyId,
+                            quantityOfQuestions = param.quantityOfQuestions - (param.quantityOfQuestions - 5)
+                        )
+                    )
+                )
+                listQuestion.addAll(
+                    tesQuestionRepository.getQuestionsList(
+                        param = GetQuestionListParam(
+                            difficultyId = param.difficultyId + 1L,
+                            quantityOfQuestions = param.quantityOfQuestions - (param.quantityOfQuestions - 3)
+                        )
+                    )
+                )
+            }
+
+            4L -> {
+                listQuestion.addAll(
+                    tesQuestionRepository.getQuestionsList(
+                        param = GetQuestionListParam(
+                            difficultyId = param.difficultyId,
+                            quantityOfQuestions = param.quantityOfQuestions - 3
+                        )
+                    )
+                )
+                listQuestion.addAll(
+                    tesQuestionRepository.getQuestionsList(
+                        param = GetQuestionListParam(
+                            difficultyId = param.difficultyId - 1L,
+                            quantityOfQuestions = param.quantityOfQuestions - (param.quantityOfQuestions - 3)
+                        )
+                    )
+                )
+            }
+        }
+        return listQuestion
     }
 }
